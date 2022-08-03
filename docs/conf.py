243516a197b1a5fba0cc9aa97cd09d2c213b568a
LSYS <lucas@lucasshen.com>
2022-08-03 20:25:02 +0800

    -

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'lexicaldiversity'
-copyright = '2022, Lucas Shen'
-author = 'Lucas Shen'
+project = 'x'
+copyright = 'zz'
+author = 'xxx'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.0.1'
+release = '.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -39,7 +39,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
